dead1838b0ec70dbc15bd7fb3a781cf41ffc712d
heysung0721@gmail.com <heysung0721@gmail.com@843f606d-82eb-7ef4-923a-79306c1fc36f>
2012-03-25 13:53:23 +0000

    完成csv文件读取

--- branch/ta-lib/com/tictactec/ta/lib/test/MyTest.java
+++ branch/ta-lib/com/tictactec/ta/lib/test/MyTest.java
@@ -1,7 +1,16 @@
 package com.tictactec.ta.lib.test;
 
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import com.dukascopy.api.IBar;
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 import com.tictactec.ta.lib.*;
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
 
 public class MyTest {
 	private double input[];
@@ -20,10 +29,21 @@ public class MyTest {
 	static public double[] Lowin = new double[]  {3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
 	static public double[] Closein = new double[]{3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
 	
-	static public double[] BeltHoldOpenin = new double[] {3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19};
-	static public double[] BeltHoldHighin = new double[] {5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,23.21};
-	static public double[] BeltHoldLowin = new double[]  {3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,18.82};
-	static public double[] BeltHoldClosein = new double[]{5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,23.10};
+	static public ArrayList<Double> Open= new ArrayList<Double>(200);
+	static public ArrayList<Double> High=new ArrayList<Double>(200);
+	static public ArrayList<Double> Low= new ArrayList<Double>(200);
+	static public ArrayList<Double> Close=new ArrayList<Double>(200);
+	
+	static public double[] gOpenin  = new double[] {3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19};
+	static public double[] gHighin  = new double[] {5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,23.21};
+	static public double[] gLowin  = new double[]  {3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,18.82};
+	static public double[] gClosein  = new double[]{5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,23.10};
+	
+	static public double[] BeltHoldOpenin = new double[200];
+	static public double[] BeltHoldHighin  = new double[200] ;
+	static public double[] BeltHoldLowin = new double[200] ;
+	static public double[] BeltHoldClosein = new double[200];
+	
 	
 	public void test()
     {
@@ -78,12 +98,54 @@ public class MyTest {
         System.out.println(retCode);
 	}
 	
+	public void arrayTodouble(){
+		int i=0;
+		for(Double O:Open){
+			BeltHoldOpenin[i]=O;
+			i++;
+		}
+		i=0;
+		for(Double H:High){
+			BeltHoldHighin[i]=H;
+			i++;
+		}
+		i=0;
+		for(Double L:Low){
+			BeltHoldLowin[i]=L;
+			i++;
+		}
+		i=0;
+		for(Double C:Close){
+			BeltHoldClosein[i]=C;
+			i++;
+		}
+//		BeltHoldOpenin
+	}
+	
+	public void copyCustom(){
+		BeltHoldOpenin=Arrays.copyOf(gOpenin, gOpenin.length);
+		BeltHoldHighin=Arrays.copyOf(gHighin, gHighin.length);
+		BeltHoldLowin=Arrays.copyOf(gLowin, gLowin.length);
+		BeltHoldClosein=Arrays.copyOf(gClosein, gClosein.length);
+		
+	}
+	
 	public static void main(String[] args) throws Exception {
 		MyTest mytest=new MyTest();
+		CSVReader reader = new CSVReader(new FileReader("opencsv-2.3/examples/15Mdata.csv"));
+		List<String[]> allElements = reader.readAll();
+		for (String[] strArray:allElements){
+			Open.add(Double.valueOf( strArray[1]));
+			High.add(Double.valueOf( strArray[2]));
+			Low.add(Double.valueOf( strArray[3]));
+			Close.add(Double.valueOf( strArray[4]));
+		}
 //		mytest.test();
 //		mytest.BeltHoldtest();
 //		mytest.cdl2CrowsTest();
 //		mytest.DarkCloudCoverTest();
+//		mytest.arrayTodouble();
+		mytest.copyCustom();
 		mytest.cdlMarubozuTest();
 	}
 
